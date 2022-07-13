985232b4da299856cb854032f37b121c65ab2fb7
Nicolas <nicolas.jouanin@netcourrier.com>
2022-07-13 18:40:09 +0200

    build: setup test configuration on CI

--- config/test.exs
+++ config/test.exs
@@ -9,10 +9,12 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :exagon, Exagon.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "exagon_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: System.get_env("POSTGRES_USER") || "postgres",
+  password: System.get_env("POSTGRES_PASSWORD") || "postgres",
+  database:
+    System.get_env("POSTGRES_DB") ||
+      "exagon_test#{System.get_env("MIX_TEST_PARTITION")}",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
