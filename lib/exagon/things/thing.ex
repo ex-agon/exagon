7e76154750c443a18b8905bd76a0b0ffffb9dc7e
Nicolas <nicolas.jouanin++github@mailo.com>
2022-08-11 21:11:48 +0400

    Rename uid to path add binding attributes column

--- lib/exagon/things/thing.ex
+++ lib/exagon/things/thing.ex
@@ -1,14 +1,16 @@
 defmodule Exagon.Things.Thing do
   use Exagon.Schema
 
+  alias Exagon.Accounts.Workplace
+
   schema "things" do
-    field :type, :string
-    field :uid, :string
+    field :path, :string
 
     field :status, Ecto.Enum,
       values: [:uninitialized, :initializing, :available, :online, :offline, :unknown]
 
     field :status_detail, :string
+    field :binding_attrs, :map
     belongs_to :workplace, Workplace
 
     timestamps()
@@ -19,5 +21,6 @@ defmodule Exagon.Things.Thing do
     thing
     |> cast(attrs, [:type, :uid, :status, :status_detail])
     |> validate_required([:type, :uid, :status])
+    |> unique_constraint(:path)
   end
 end
--- priv/repo/migrations/20220806134851_create_things.exs
+++ priv/repo/migrations/20220806134851_create_things.exs
@@ -4,11 +4,14 @@ defmodule Exagon.Repo.Migrations.CreateThings do
   def change do
     create table(:things, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :uid, :string, null: false
+      add :path, :string, null: false
       add :status, :string, null: false
       add :status_detail, :string
+      add :binding_attrs, :map, default: %{}
 
       timestamps()
     end
+
+    unique_index(:things, :path)
   end
 end
